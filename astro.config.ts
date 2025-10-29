a5173b958d1ecc0bbf9d25c0e161736a2d084b9b
achuanya <haibao1027@gmail.com>
2025-10-29 19:53:58 +0800

    feat: 新版本准备上线

--- astro.config.ts
+++ astro.config.ts
@@ -166,13 +166,13 @@ export default defineConfig({
   prefetch: {
     prefetchAll: true,
   },
-  image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "constrained",
-  },
-  experimental: {
-    responsiveImages: true,
-    preserveScriptOrder: true,
-  },
+  // image: {
+  //   // Used for all Markdown images; not configurable per-image
+  //   // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
+  //   experimentalLayout: "constrained",
+  // },
+  // experimental: {
+  //   responsiveImages: true,
+  //   preserveScriptOrder: true,
+  // },
 });
